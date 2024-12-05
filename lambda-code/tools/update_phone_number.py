6180b3a9a89f808a1c359e9f27616872ed2ef85e
Patrick Dorrian <pathugod@gmail.com>
2024-12-05 18:39:22 +0000

    Update phone number tool

---
+++ lambda-code/tools/update_phone_number.py
@@ -0,0 +1,25 @@
+import lambda_function
+
+
+class update_phone_number:
+    @staticmethod
+    def run(instance, phone_number):
+        lambda_function.response['phone_number'] = phone_number
+
+    description = {
+        "type": "function",
+        "function": {
+            "name": "update_phone_number",
+            "description": "Update the user's phone number.",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "new_recipient": {
+                        "type": "string",
+                        "description": "The phone number of the user."
+                    }
+                },
+                "required": ["phone_number"]
+            }
+        }
+    }
